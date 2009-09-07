f8db95ecf4f663fc697d0aae75073a141121ca25
cladden <christopher.ladden@gmail.com>
2009-09-07 15:41:29 -0400

    The reference panel now supports HTML.

--- antipasto/GUI/GadgetListView/Wing.java
+++ antipasto/GUI/GadgetListView/Wing.java
@@ -56,300 +56,294 @@ import antipasto.Util.Utils;
 import processing.app.*;
 
 public class Wing extends JDialog implements ComponentListener,
-		IActiveGadgetChangedEventListener, FocusListener, ISelectedItemListener {
+IActiveGadgetChangedEventListener, FocusListener, ISelectedItemListener {
 
-	// The standard width and height for the dialog
-	private int cachedHeight = 425;
-	private int cachedWidth = 300;
-	
-	private JFrame component;
-	private IModule activeModule;
+    // The standard width and height for the dialog
+    private int cachedHeight = 425;
+    private int cachedWidth = 300;
 
-	private JPanel	   wingHeader;
-	private JPanel	   wingBody;
-	private WingFooter wingFooter;
-	
-	private WingPanelReference wingPanelReference;
-	private WingPanelScripts   wingPanelScripts;
-	//private WingPanelAppStore wingPanelAppStore;
-	
-	private String[]  wingTabNames = {""};
-	private WingTab[] wingTabs   = {null,null};
-	private JPanel[]  wingPanels = {null,null};
-	private int		  wingFocusedIndex = 0;
-	
-	public Wing(JFrame parent) {
-		super(parent, false);
-		component = parent;
-		this.setUndecorated(true);
-		this.init();
-		parent.addComponentListener(this);
-		this.addComponentListener(this);
-	}
-	
-	/**
-	 * Initialize a Wing Header
-	 *  Returns: a panel with the Tabs populated for the right wing.
-	 */
-	JPanel initWingHeader(String[] tabNames) {
+    private JFrame component;
+    private IModule activeModule;
 
-		JPanel bgPanel = new JPanel();
-		bgPanel.setSize(cachedWidth, 15);
-		bgPanel.setBackground(new Color(0x21, 0x68, 0x86));
-		bgPanel.setLayout(new BorderLayout());
-		
-		/* Create the tab Panel */
-		JPanel tabPanel = new JPanel();
-		tabPanel.setBackground(new Color(0x21, 0x68, 0x86));
-		tabPanel.setSize(cachedWidth, 15);
-		tabPanel.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
-		tabPanel.setOpaque(true);
-		
-		/* Create the tabs */
-		wingTabNames = tabNames.clone();
-		for (int x=0; x<wingTabNames.length; x++) {
-			wingTabs[x] = new WingTab(wingTabNames[x],null);
-			wingTabs[x].addSelectionListener(this);
-			tabPanel.add(wingTabs[x]);
-		}
-		
-		/* A Horizontal Separator */
-		JComponent hrImg = new JComponent() {
-									private static final long serialVersionUID = 1L;
+    private JPanel     wingHeader;
+    private JPanel     wingBody;
+    private WingFooter wingFooter;
 
-									public void paint(Graphics g) {
-							 	        Graphics2D graphics2 = (Graphics2D) g;
-							 	        graphics2.setBackground(new Color(255,255,255));
-							 	        graphics2.setColor(new Color(255,255,255));
-							 	        graphics2.drawLine(0,0,700,1);
-							       	 }};
-		
-		bgPanel.add(tabPanel, BorderLayout.NORTH);					       	 
-		bgPanel.add(hrImg, BorderLayout.CENTER);	
-		
-		return bgPanel;
-	}
-	
-	/*************************************************
-	 * Initialize a Test Panel
-	 *  Returns: The test panel
-	 */
-	JPanel initTestPanel(String message) {
+    private WingPanelReference wingPanelReference;
+    private WingPanelScripts   wingPanelScripts;
+    //private WingPanelAppStore wingPanelAppStore;
 
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
-		panel.setLayout(new BorderLayout());
-		panel.setOpaque(true);
-		
-		JLabel label = new JLabel(message);
-		label.setForeground(new Color(0, 0, 0));
-		
-		JPanel headerPanel = initTestPanelHeader(message);
-		
-		// Assemble the panel
-		panel.add(headerPanel, BorderLayout.NORTH);
-		panel.add(label, BorderLayout.CENTER);
-		return panel;
-	}
-	
+    private String[]  wingTabNames = {""};
+    private WingTab[] wingTabs   = {null,null};
+    private JPanel[]  wingPanels = {null,null};
+    private int       wingFocusedIndex = 0;
 
-	/**
-	 * Initialize a Test Panel Header
-	 *  Returns: a JPanel with a header text 
-	 */
-	JPanel initTestPanelHeader(String textDisplay) {
-		
-		JPanel headerPanel = new JPanel();
-		headerPanel.setBackground(new Color(0x04, 0x4F, 0x6F));
-		headerPanel.setLayout(new BorderLayout());
+    public Wing(JFrame parent) {
+        super(parent, false);
+        component = parent;
+        this.setUndecorated(true);
+        this.init();
+        parent.addComponentListener(this);
+        this.addComponentListener(this);
+    }
 
-		JLabel headerLabel = new JLabel(textDisplay);
-		
-		headerLabel.setForeground(new Color(0xFF, 0xFF, 0xFF));
-		headerPanel.add(headerLabel, BorderLayout.CENTER);		
-		
-		return headerPanel;
-	}
-	
+    /**
+     * Initialize a Wing Header
+     *  Returns: a panel with the Tabs populated for the right wing.
+     */
+    JPanel initWingHeader(String[] tabNames) {
 
-	/*************************************************
-	 * Initialize the Wing Body
-	 * 
-	 * The Wing Body always returns:     --------------
-	 * 								      Header Panel
-	 * 						     	      Content Panel
-	 *								     ---------------
-	 */ 								
-	JPanel initWingBody() {
-		
-		
-		  /* Build each panel */
-		  wingPanelReference =  new WingPanelReference("This is the reference panel", 
-							    						wingFooter,
-							    						cachedWidth,
-							    						cachedHeight);
-		  
-		  String scriptPath = new String(Sketchbook.getSketchbookPath() + File.separator); 
-		  wingPanelScripts = new WingPanelScripts(	scriptPath, 
-				  							   		wingFooter, 
-				  							   		cachedWidth, 
-				  							   		cachedHeight);
-		  
-		  //wingPanelAppStore =  new WingPanelAppStore(	wingFooter,
-		  //											cachedWidth,
-		  //											cachedHeight);
+        JPanel bgPanel = new JPanel();
+        bgPanel.setSize(cachedWidth, 15);
+        bgPanel.setBackground(new Color(0x21, 0x68, 0x86));
+        bgPanel.setLayout(new BorderLayout());
 
-		  wingPanels[0] = wingPanelReference;
-		  wingPanels[1] = wingPanelScripts;
-		  //wingPanels[2] = wingPanelAppStore;
-		  //wingPanels[3] = initTestPanel("Test2");
-		  
-		/* Retrieve the previous focused index */
-		try {
-			wingFocusedIndex = Preferences.getInteger("editor.rightWing.selectedTabIndex");
-		} catch (Exception e) {
-			wingFocusedIndex = 0;
-		}
-		
-		// Error checking
-		if (wingFocusedIndex >= wingTabs.length ) {
-			wingFocusedIndex = 0;
-		}
-		
-		wingTabs[wingFocusedIndex].setFocused(true); //focus the tab
-		return wingPanels[wingFocusedIndex];
-		  
-	}
+        /* Create the tab Panel */
+        JPanel tabPanel = new JPanel();
+        tabPanel.setBackground(new Color(0x21, 0x68, 0x86));
+        tabPanel.setSize(cachedWidth, 15);
+        tabPanel.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+        tabPanel.setOpaque(true);
 
-	/*************************************************
-	 * Initialize the Right Wing
-	 * 
-	 * The Right Wing always has:   --------------
-	 * 								  Tab Panel
-	 * 						     	  Content Panel
-	 * 							      Status Panel
-	 * 								---------------
-	 */
-	private void init() {
+        /* Create the tabs */
+        wingTabNames = tabNames.clone();
+        for (int x=0; x<wingTabNames.length; x++) {
+            wingTabs[x] = new WingTab(wingTabNames[x],null);
+            wingTabs[x].addSelectionListener(this);
+            tabPanel.add(wingTabs[x]);
+        }
 
-		this.getContentPane().setLayout(new BorderLayout());
-		this.setSize(cachedWidth, cachedHeight);
-		this.setBackground(new Color(0x04, 0x4F, 0x6F));
-		
-		
-		wingHeader = initWingHeader(new String[] {"Reference", "Scripts"});
-		wingFooter = new WingFooter(" Scripts loaded.", cachedWidth, 15);
-		wingBody   = initWingBody();
-		
-		
-		this.getContentPane().add(wingBody, BorderLayout.CENTER);
-		this.getContentPane().add(wingHeader, BorderLayout.NORTH);
-		this.getContentPane().add(wingFooter, BorderLayout.SOUTH);
+        /* A Horizontal Separator */
+        JComponent hrImg = new JComponent() {
+            private static final long serialVersionUID = 1L;
 
-	}
+            public void paint(Graphics g) {
+                Graphics2D graphics2 = (Graphics2D) g;
+                graphics2.setBackground(new Color(255,255,255));
+                graphics2.setColor(new Color(255,255,255));
+                graphics2.drawLine(0,0,700,1);
+            }
+        };
 
-	
-	private void setLocation() {
-		if (this.isVisible() && ((Editor) component).centerPanel.isVisible()) {
-			int xLocation = component.getX() + component.getWidth();
-			int yLocation = ((Editor) component).centerPanel
-					.getLocationOnScreen().y;
-			this.setLocation(xLocation, yLocation);
-		}
-	}
+        bgPanel.add(tabPanel, BorderLayout.NORTH);
+        bgPanel.add(hrImg, BorderLayout.CENTER);
 
-	// Component Listener: This is designed to listen specifically to the editor
-	// windows so that we can adjust our size according to
-	// it for symmetry...specifically with the gadget panel
-	public void componentHidden(ComponentEvent arg0) {
+        return bgPanel;
+    }
 
-	}
+    /*************************************************
+     * Initialize a Test Panel
+     *  Returns: The test panel
+     */
+    JPanel initTestPanel(String message) {
 
-	public void componentMoved(ComponentEvent arg0) {
-		setLocation();
-		repaint();
-	}
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(255, 255, 255));
+        panel.setLayout(new BorderLayout());
+        panel.setOpaque(true);
 
-	public void componentResized(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
+        JLabel label = new JLabel(message);
+        label.setForeground(new Color(0, 0, 0));
 
-	}
+        JPanel headerPanel = initTestPanelHeader(message);
 
-	public void componentShown(ComponentEvent arg0) {
-		setLocation();
-		repaint();
-	}
+        // Assemble the panel
+        panel.add(headerPanel, BorderLayout.NORTH);
+        panel.add(label, BorderLayout.CENTER);
+        return panel;
+    }
 
-	public void onActiveGadgetChanged(ActiveGadgetObject obj) {
-		if (obj != null) {
-			this.activeModule = obj.getModule();
-			if (this.activeModule.getReferenceText() != null
-					|| this.activeModule.getReferenceText()
-							.equalsIgnoreCase("")) {
-				
-				// Disabled until reference text is written..
-				// wingPanelReference.setText(this.activeModule.getReferenceText());
-				wingPanelReference.setText(this.activeModule.getName() + " reference text here.");
-			} else {
-				System.out.println("No reference text found, defaulting ");
-				wingPanelReference.setText(this.activeModule.getName() + " reference text here.");
-			}
-		} else {
-			wingPanelReference.setText("Reference text area.");
-		}
-	}
 
-	public void focusGained(FocusEvent arg0) {
+    /**
+     * Initialize a Test Panel Header
+     *  Returns: a JPanel with a header text
+     */
+    JPanel initTestPanelHeader(String textDisplay) {
 
-	}
+        JPanel headerPanel = new JPanel();
+        headerPanel.setBackground(new Color(0x04, 0x4F, 0x6F));
+        headerPanel.setLayout(new BorderLayout());
 
-	public void focusLost(FocusEvent arg0) {
-		
-		/* Disabled until Omar needs this ;-)
-		if (this.activeModule != null) {
-			this.activeModule.setReferenceText(wingPanelReference.getText());
-			System.out.println("setting the text");
-		} else {
-			System.out.println("Text being set to null");
-		}
-		*/
-	}
+        JLabel headerLabel = new JLabel(textDisplay);
 
-	/**
-	 * Tab Selection Control. 
-	 * This event handles the tab and panel selection changes.
-	 */
-	public void onSelected(Object object) {
-		// TODO Auto-generated method stub
-		WingTab selected = (WingTab)object;
-		
-		/* A tab was selected, update the focus of all items */
-		for (int x=0; x<wingTabs.length; x++) {
-			
-			if (wingTabs[x].txt == selected.txt) {
+        headerLabel.setForeground(new Color(0xFF, 0xFF, 0xFF));
+        headerPanel.add(headerLabel, BorderLayout.CENTER);
 
-				/* Enable the other tabs and panels */
-				wingFocusedIndex = x;			//Store the focused Id
-				Preferences.setInteger("editor.rightWing.selectedTabIndex", wingFocusedIndex);
-				wingTabs[x].setFocused(true);
-				
-				/* Load a new Body panel */
-				wingBody = wingPanels[x];
-				this.getContentPane().add(wingBody, BorderLayout.CENTER);
-				wingPanels[x].setEnabled(true);
-				wingPanels[x].setVisible(true);
-				
-			} else {
-				
-				 /* Disable the other tabs and Body panels */
-				 wingTabs[x].setFocused(false);
-				 wingPanels[x].setEnabled(false);
-				 wingPanels[x].setVisible(false);
-			}
-			
-		}
-		
-	}
-	
+        return headerPanel;
+    }
+
+
+    /*************************************************
+     * Initialize the Wing Body
+     *
+     * The Wing Body always returns:     --------------
+     * 				                      Header Panel
+     * 					                  Content Panel
+     *                                   --------------
+     */
+    JPanel initWingBody() {
+
+
+        /* Build each panel */
+        wingPanelReference =  new WingPanelReference("http://livingcomputation.com/s/doc/examples.html",
+                                                     wingFooter,
+                                                     cachedWidth,
+                                                     cachedHeight);
+
+        String scriptPath = new String(Sketchbook.getSketchbookPath() + File.separator);
+        wingPanelScripts = new WingPanelScripts(scriptPath,
+                                                wingFooter,
+                                                cachedWidth,
+                                                cachedHeight);
+
+        //wingPanelAppStore =  new WingPanelAppStore(	wingFooter,
+        //											cachedWidth,
+        //											cachedHeight);
+
+        wingPanels[0] = wingPanelReference;
+        wingPanels[1] = wingPanelScripts;
+        //wingPanels[2] = wingPanelAppStore;
+        //wingPanels[3] = initTestPanel("Test2");
+
+        /* Retrieve the previous focused index */
+        try {
+            wingFocusedIndex = Preferences.getInteger("editor.rightWing.selectedTabIndex");
+        } catch (Exception e) {
+            wingFocusedIndex = 0;
+        }
+
+        // Error checking
+        if (wingFocusedIndex >= wingTabs.length ) {
+            wingFocusedIndex = 0;
+        }
+
+        wingTabs[wingFocusedIndex].setFocused(true); //focus the tab
+        return wingPanels[wingFocusedIndex];
+
+    }
+
+    /*************************************************
+     * Initialize the Right Wing
+     *
+     * The Right Wing always has:   --------------
+     * 								  Tab Panel
+     * 						     	  Content Panel
+     * 							      Status Panel
+     * 								---------------
+     */
+    private void init() {
+
+        this.getContentPane().setLayout(new BorderLayout());
+        this.setSize(cachedWidth, cachedHeight);
+        this.setBackground(new Color(0x04, 0x4F, 0x6F));
+
+
+        wingHeader = initWingHeader(new String[] {"Reference", "Scripts"});
+        wingFooter = new WingFooter(" Scripts loaded.", cachedWidth, 15);
+        wingBody   = initWingBody();
+
+
+        this.getContentPane().add(wingBody, BorderLayout.CENTER);
+        this.getContentPane().add(wingHeader, BorderLayout.NORTH);
+        this.getContentPane().add(wingFooter, BorderLayout.SOUTH);
+
+    }
+
+
+    private void setLocation() {
+        if (this.isVisible() && ((Editor) component).centerPanel.isVisible()) {
+            int xLocation = component.getX() + component.getWidth();
+            int yLocation = ((Editor) component).centerPanel
+                            .getLocationOnScreen().y;
+            this.setLocation(xLocation, yLocation);
+        }
+    }
+
+    // Component Listener: This is designed to listen specifically to the editor
+    // windows so that we can adjust our size according to
+    // it for symmetry...specifically with the gadget panel
+    public void componentHidden(ComponentEvent arg0) {
+
+    }
+
+    public void componentMoved(ComponentEvent arg0) {
+        setLocation();
+        repaint();
+    }
+
+    public void componentResized(ComponentEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void componentShown(ComponentEvent arg0) {
+        setLocation();
+        repaint();
+    }
+
+    public void onActiveGadgetChanged(ActiveGadgetObject obj) {
+        if (obj != null) {
+            this.activeModule = obj.getModule();
+            if (this.activeModule.getReferenceText() != null
+                || this.activeModule.getReferenceText()
+                .equalsIgnoreCase("")) {
+                wingPanelReference.setPage("http://www.google.com");
+                // Disabled until reference text is written..
+                // wingPanelReference.setText(this.activeModule.getReferenceText());
+                //wingPanelReference.setText(this.activeModule.getName() + " reference text here.");
+            } else {
+                System.out.println("No reference text found, defaulting ");
+                //wingPanelReference.setText(this.activeModule.getName() + " reference text here.");
+            }
+        } else {
+            wingPanelReference.setPage("http://www.google.com");
+            //wingPanelReference.setText("Reference text area.");
+        }
+    }
+
+    public void focusGained(FocusEvent arg0) {
+
+    }
+
+    public void focusLost(FocusEvent arg0) {
+
+    }
+
+    /**
+     * Tab Selection Control.
+     * This event handles the tab and panel selection changes.
+     */
+    public void onSelected(Object object) {
+        // TODO Auto-generated method stub
+        WingTab selected = (WingTab)object;
+
+        /* A tab was selected, update the focus of all items */
+        for (int x=0; x<wingTabs.length; x++) {
+
+            if (wingTabs[x].txt == selected.txt) {
+
+                /* Enable the other tabs and panels */
+                wingFocusedIndex = x;           //Store the focused Id
+                Preferences.setInteger("editor.rightWing.selectedTabIndex", wingFocusedIndex);
+                wingTabs[x].setFocused(true);
+
+                /* Load a new Body panel */
+                wingBody = wingPanels[x];
+                this.getContentPane().add(wingBody, BorderLayout.CENTER);
+                wingPanels[x].setEnabled(true);
+                wingPanels[x].setVisible(true);
+
+            } else {
+
+                /* Disable the other tabs and Body panels */
+                wingTabs[x].setFocused(false);
+                wingPanels[x].setEnabled(false);
+                wingPanels[x].setVisible(false);
+            }
+
+        }
+
+    }
+
 
 }
--- antipasto/GUI/GadgetListView/WingPanelReference.java
+++ antipasto/GUI/GadgetListView/WingPanelReference.java
@@ -1,4 +1,6 @@
 package antipasto.GUI.GadgetListView;
+import javax.swing.JEditorPane;
+import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -16,6 +18,8 @@ import java.awt.geom.RoundRectangle2D;
 import java.io.*;
 import java.net.*;
 import java.util.Properties;
+import java.util.*;
+import javax.swing.text.html.*;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -23,10 +27,13 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.event.EventListenerList;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
 
 import processing.app.Base;
 import processing.app.Sketchbook;
@@ -37,32 +44,35 @@ import antipasto.Interfaces.*;
 import antipasto.Util.Utils;
 
 public class WingPanelReference extends JPanel {
-	
+
 	boolean isUpdated = false;
 	String text;
 	Font font;
-	
+
 	private Color bgDefaultColor =Color.WHITE;
 	private Color bgHoverColor = Color.WHITE;
 	private Color bgColor;
-	
+
 	private Color textDefaultColor = Color.BLACK;
 	private Color textHoverColor = Color.WHITE;
 	private Color textColor;
-	
-	private double width = 300; 
+
+	private double width = 300;
 	private double height = 450;
 	private boolean isFocused = false;
 	private boolean isSelected = false;
-	
-	private String referenceFile; 
-	
+
+	private String referenceFile;
+
 	private JPanel     headerPanel;
 	private JLabel     headerLabel;
 	private JScrollPane scrollPane;
 	private WingFooter  wingFooter;
-	private JTextArea textArea;
-	
+	//final JEditorPane textArea = new JEditorPane();
+	final JTextPane textArea = new JTextPane();
+	final List <URL> htmlHistory = new ArrayList <URL>();
+
+
 	/**
 	 * Creates a new Wing Panel for Reference
 	 * @param text The text string that contains the reference.
@@ -70,107 +80,142 @@ public class WingPanelReference extends JPanel {
 	 * @param width The width of the panel
 	 * @param height The height of the panel
 	 */
-	public WingPanelReference(String text,
-							  Object wingFooter, 
-							  int width, int height){
+	public WingPanelReference(String url,
+				  Object wingFooter,
+				  int width, int height){
 
-		this.wingFooter = (WingFooter)wingFooter;
-		this.text = text;
-		this.width = width;
-		this.height = height;
+	    this.wingFooter = (WingFooter)wingFooter;
+	    this.text = text;
+	    this.width = width;
+	    this.height = height;
 
-		
-		/* Setup the panel */
-		setSize(width, height);
-		setBackground(bgDefaultColor);
-		setLayout(new BorderLayout());
-		
-		/* Build the header */
-		headerPanel = initPanelHeader(" <- Back  ");
-		
-		/* Build the body */
-		initBody();
-		
-		/* Assemble the Panel */
-		add(headerPanel, BorderLayout.NORTH);
-		add(scrollPane, BorderLayout.CENTER);
+
+	    /* Setup the panel */
+	    setSize(width, height);
+	    setBackground(bgDefaultColor);
+	    setLayout(new BorderLayout());
+
+	    /* Build the header */
+	    headerPanel = initPanelHeader("<html><u>&lt;-Back </u></html>");
+
+	    /* Build the body */
+	    initBody();
+
+	    /* Assemble the Panel */
+	    add(headerPanel, BorderLayout.NORTH);
+	    add(scrollPane, BorderLayout.CENTER);
+
+	    /* Set the page*/
+	    setPage(url);
 	}
-	
+
 	/**
 	 * Initialize a Panel Header
 	 * @param textDisplay The button text
 	 * @return: a JPanel with a button
 	 */
 	JPanel initPanelHeader(String textDisplay) {
-		
-		JPanel headerPanel = new JPanel();
-		headerPanel.setBackground(new Color(0x04, 0x4F, 0x6F));
-		headerPanel.setLayout(new BorderLayout());
-		
-		headerLabel = new JLabel(textDisplay);
-		
-		headerLabel.addMouseListener(new MouseListener() {
 
-			public void mouseClicked(MouseEvent arg0) { 
-				;
-			}
+	    JPanel headerPanel = new JPanel();
+	    headerPanel.setBackground(new Color(0x04, 0x4F, 0x6F));
+	    headerPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0,0));
 
-			public void mouseEntered(MouseEvent arg0) {
-			
-				headerLabel.setForeground(Color.orange);
-				
-				wingFooter.setText(" Go Back ");
-				
-			}
+	    headerLabel = new JLabel(textDisplay);
 
-			public void mouseExited(MouseEvent arg0) {
-				headerLabel.setForeground(Color.white);
-				wingFooter.setText(" ");
-			}
+	    MouseListener mouseListener = new MouseListener() {
+		    public void mouseClicked(MouseEvent arg0) {
 
-			public void mousePressed(MouseEvent arg0) {
-				
-					wingFooter.setText("Ready.");
-			}
+			    try {
+				//Go Back
+				if ( (htmlHistory.size()-1) > 0) {
+				    htmlHistory.remove(htmlHistory.size()-1);
+				    setPage(htmlHistory.get(htmlHistory.size()-1).toString());
+				}
+			    } catch (Exception ex) {
+				ex.printStackTrace();
+			    }
 
-			public void mouseReleased(MouseEvent arg0) {
+		    }
 
-			}
-			
-			});
-		
-		headerLabel.setForeground(new Color(0xFF, 0xFF, 0xFF));
-		headerPanel.add(headerLabel, BorderLayout.CENTER);		
-		
-		return headerPanel;
+		    public void mouseEntered(MouseEvent arg0) {
+
+			    headerLabel.setForeground(Color.orange);
+
+			    wingFooter.setText(" Go Back ");
+
+		    }
+
+		    public void mouseExited(MouseEvent arg0) {
+			    headerLabel.setForeground(Color.white);
+			    wingFooter.setText(" ");
+		    }
+
+		    public void mousePressed(MouseEvent arg0) {
+
+		    }
+
+		    public void mouseReleased(MouseEvent arg0) {
+
+		    }
+
+		};
+
+
+	    headerLabel.addMouseListener(mouseListener);
+
+	    headerLabel.setForeground(new Color(0xFF, 0xFF, 0xFF));
+	    headerPanel.add(headerLabel);
+
+	    return headerPanel;
 	}
-	
+
 	/**
 	 * Initialize the body of the panel.
 	 */
 	private void initBody() {
-		
-		textArea = new JTextArea(text);
-		scrollPane = new JScrollPane(textArea);
-		scrollPane
-				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane
-				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-		textArea.setWrapStyleWord(true);
-		textArea.setLineWrap(true);
-		textArea.setBounds(scrollPane.getBounds());
+            HyperlinkListener listener = new HyperlinkListener() {
+              public void hyperlinkUpdate(HyperlinkEvent e) {
+                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                    setPage(e.getURL().toString());
+                }
+              }
+            };
+            textArea.addHyperlinkListener(listener);
+            textArea.setVisible(true);
+            textArea.setEditable(false);
 
-		textArea.setVisible(true);
-		scrollPane.setVisible(true);
+            scrollPane = new JScrollPane(textArea);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            scrollPane.setVisible(true);
 	}
-	
+
 	/**
-	 * 
-	 * @param text The reference text to display.
+	 *
+	 * @param url - The url of the page to display.
 	 */
-	public void setText(String text) {
-		textArea.setText(text);
+	public void setPage(String url) {
+	    try {
+		textArea.setPage(url);
+		htmlHistory.add(textArea.getPage());
+		System.out.println(htmlHistory.get(htmlHistory.size()-1));
+	    } catch (IOException ioe) {
+		System.out.println("Error Loading: " + ioe);
+	    }
 	}
-	
+
+	/**
+	 * Clears html page history in the docs browser. Usually called
+	 * when switching boards.
+	 *
+	 * @author Chris (9/7/2009)
+	 */
+	public void clearHistory(){
+	    //Clear the history
+	    while(htmlHistory.size() > 0) {
+		htmlHistory.remove(htmlHistory.size() - 1);
+	    }
+	}
+
 }
